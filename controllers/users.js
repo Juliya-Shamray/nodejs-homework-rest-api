9b01d30aa371994c12d3010a032e5b1aefc73bae
Juliya Shamray <fizmatka908990@gmail.com>
2023-12-11 11:25:21 +0200

    done

--- controllers/users.js
+++ controllers/users.js
@@ -3,8 +3,14 @@ const ctrlWrapper = require("../helpers/ctrlWrapper");
 const HttpError = require("../helpers/HttpError");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const gravatar = require("gravatar");
+const path = require("path");
+const Jimp = require("jimp");
+const fs = require("fs/promises");
 
 const { SECRET_KEY } = process.env;
+
+const avatarDir = path.join(__dirname, "../", "public", "avatars");
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -14,7 +20,13 @@ const register = async (req, res) => {
   }
 
   const hashPassword = await bcrypt.hash(password, 10);
-  const newUser = await User.create({ ...req.body, password: hashPassword });
+  const avatarURL = gravatar.url(email);
+
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL,
+  });
 
   res.status(201).json({
     user: {
@@ -76,10 +88,32 @@ const subscription = async (req, res) => {
     subscription: user.subscription,
   });
 };
+
+const updateAvatar = async (req, res) => {
+  const { _id } = req.user;
+  const { path: tmpUpload, originalname } = req.file;
+
+  const image = await Jimp.read(tmpUpload);
+  const resizedImage = image.resize(250, 250);
+
+  await resizedImage.writeAsync(tmpUpload);
+
+  const fileName = `${_id}_${originalname}`;
+  const resultDir = path.join(avatarDir, fileName);
+
+  await fs.rename(tmpUpload, resultDir);
+
+  const avatarURL = path.join("avatars", fileName);
+
+  await User.findByIdAndUpdate(_id, { avatarURL });
+
+  res.json({ avatarURL });
+};
 module.exports = {
   register: ctrlWrapper(register),
   login: ctrlWrapper(login),
   getCurrent: ctrlWrapper(getCurrent),
   logout: ctrlWrapper(logout),
   subscription: ctrlWrapper(subscription),
+  updateAvatar: ctrlWrapper(updateAvatar),
 };
